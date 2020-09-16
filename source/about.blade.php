ca73deb10c206c62370444b60275a427e5eacea9
jdm79 <james.malvern@gmail.com>
2020-09-16 22:14:26 +0200

    add about details

--- source/about.blade.php
+++ source/about.blade.php
@@ -7,10 +7,29 @@
 
     <p>My name is {{ $page->owner->name }}</p>
 
+    <h1>Interests</h1>
+    <ul>
+      <li>Twitter bots</li>
+      <li>Web scraping</li>
+      <li>Data journalism</li>
+      <li>Python</li>
+      <li>Javascript</li>
+      <li>Web-scraping Twitter bots Javascript</li>
+    </ul>
+R
+
     <h2>Links:</h2>
 
     <ul>
         <li><a href="https://twitter.com/{{ $page->owner->twitter }}" target="_blank">Twitter</a></li>
+        <li><a href="https://twitter.com/HansardLord" target="_blank">Twitter</a>
+        Hansard Lord (Twitter bot listing what they get up to every day in the House of Lords. 
+        This bot was chosen to be part of 
+        <a href="https://blog.twitter.com/developer/en_us/topics/tools/2020/introducing_new_twitter_api.html">
+        Twitter's showcase of Bots</a> that share information 
+        and make conversations more fun using the Twitter API)</li>
         <li><a href="https://github.com/{{ $page->owner->github }}" target="_blank">GitHub</a></li>
+        <li><a href="https://uk-headline-checker.herokuapp.com" 
+        target="_blank">UK Headline Checker (uses a free Heroku dyno, so will take a while to wake up initially)</a></li>
     </ul>
 @endsection
