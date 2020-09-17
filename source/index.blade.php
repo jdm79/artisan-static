e34a6eea9eebfa71f68e4c22a11617544adcc89b
jdm79 <james.malvern@gmail.com>
2020-09-17 13:36:57 +0200

    reduce home blurb

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <p><i class="fas fa-map-marker-alt"></i> Berlin, Germany</p>
 
-    <p>Data, words and coding | @cityjournalism @cityjsconf. Owner of Twitter bots, including 
+    <p>Data, words and coding. Owner of Twitter bots, including 
     <a href="https://twitter.com/HansardLord">@QuotesTap</a> and <a href="https://twitter.com/HansardLord">@HansardLord</a>.
  And the <a href="http://uk-headline-checker.herokuapp.com">UK Headline Checker website.</a></p>
 
