567a01150ecd2e17d185688d79c86b481cdb6d8d
jdm79 <james.malvern@gmail.com>
2020-09-17 12:40:19 +0200

    tidy up footer

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -40,8 +40,8 @@
             <small>
                 &copy; <span data-year></span> {{ $page->owner->name }} &nbsp;&bull;&nbsp;
                 <!-- <a href="/feed.atom">RSS</a> &nbsp;&bull;&nbsp; -->
-                <!-- Te by Ranie Santos &nbsp;&bull;&nbsp;
-                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>. -->
+                Artisan Static template by Ranie Santos &nbsp;&bull;&nbsp;
+                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>.
             </small>
         </footer>
     </section>
