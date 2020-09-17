e1f763fb3f6d2d1f4d7f0a880e8169bb09ec1d8f
jdm79 <james.malvern@gmail.com>
2020-09-17 12:54:10 +0200

    add new favicons, add email address on main page

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,6 +7,8 @@
     <p>Data, words and coding | @cityjournalism @cityjsconf. Owner of Twitter bots, including 
     <a href="https://twitter.com/HansardLord">@QuotesTap</a> and <a href="https://twitter.com/HansardLord">@HansardLord</a>.
  And the <a href="http://uk-headline-checker.herokuapp.com">UK Headline Checker website.</a></p>
+
+    <p>Email: <a href = "mailto: james.malvern@city.ac.uk">james.malvern at city.ac.uk</a></p>
 @endsection
 
 
