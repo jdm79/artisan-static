b23db26a8db091ff2f46a6314379a7d30c815bc7
jdm79 <james.malvern@gmail.com>
2021-01-03 20:59:38 +0100

    fix email thing

--- source/about.blade.php
+++ source/about.blade.php
@@ -11,7 +11,7 @@
     <hr>
 
     <ul>
-        <li><a href = "mailto:james.malvern@city.ac.uk">Email me</a></li>
+        <li><a href ="mailto:james.malvern@city.ac.uk">Email me</a></li>
         <li><a href="https://twitter.com/{{ $page->owner->twitter }}" target="_blank">My personal Twitter account</a></li>
         <li><a href="https://www.linkedin.com/in/james-malvern/" target="_blank">LinkedIn</a></li>
         <li><a href="https://github.com/{{ $page->owner->github }}" target="_blank">GitHub</a></li>
